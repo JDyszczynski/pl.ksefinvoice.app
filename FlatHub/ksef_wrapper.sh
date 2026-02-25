221a3be60ff0bcb3c32bc67b37e58854dd3bb320
Jarosław Dyszczyński <ksefinvoice@example.com>
2026-02-25 22:20:31 +0100

    Add theme integration permissions and logic

--- FlatHub/ksef_wrapper.sh
+++ FlatHub/ksef_wrapper.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/app/lib/ksef:$LD_LIBRARY_PATH
 
+# Próba integracji motywu
+export QT_QPA_PLATFORMTHEME=gtk3
+
 # Katalog na bazę danych i konfigurację użytkownika - zgodnie z XDG
 DATA_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/KsefInvoice"
 
