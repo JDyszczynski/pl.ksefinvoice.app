0d4a4fb7f89353c7206961eb0dd5e8b0bf571116
Jarosław Dyszczyński <jarek@dyszczynski.pl>
2026-03-02 18:28:41 +0100

    revers portals due to err

--- FlatHub/ksef_wrapper.sh
+++ FlatHub/ksef_wrapper.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Wymuś na Qt używanie portali dla dialogów i motywów
-export QT_QPA_PLATFORM="wayland;xcb"
-export QT_QPA_PLATFORMTHEME=xdg-desktop-portal
-
 # Prostszy wrapper bez LD_LIBRARY_PATH
 DATA_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/KsefInvoice"
 mkdir -p "$DATA_DIR"
